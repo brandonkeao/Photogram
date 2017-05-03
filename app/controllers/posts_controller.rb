8e2d2b0d2be39be8e2e2bca05d9ddf586b9baf9a
bkeao <brandon@orainteractive.com>
2017-05-02 22:13:59 -0500

    Added capability to delete and edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,32 @@ class PostsController < ApplicationController
   	redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private 
 
   def post_params
   	params.require(:post).permit(:image, :caption)
   end
+
+
 
 end
